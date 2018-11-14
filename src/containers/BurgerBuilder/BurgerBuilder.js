b25b74a90d1f98da5d6c57792aa0b7ad9b9c9268
CalvusCaput <CalvusCaput@yandex.ru>
2018-11-14 20:04:48 +0300

    Added all routing

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -84,29 +84,16 @@ componentDidMount() {
     this.setState({purchasing: false})
   }
   purchaseContinueHandler = () => {
-    // this.setState({loading: true});
-    // const order = {
-    //   ingredients: this.state.ingredients,
-    //   price: this.state.totalPrice,
-    //   custromer: {
-    //     name: 'Max swarz',
-    //     address: {
-    //       street: 'teststreet 1',
-    //       zipcode: '46543',
-    //       country: 'Germany'
-    //     },
-    //     email: 'test@test.com'
-    //   },
-    //   deliveryMethod: 'fastest'
-    // }
-    // axios.post('/orders.json', order)
-    // .then(response => {
-    //   this.setState({loading: false, purchasing: false})
-    // })
-    // .catch(error => {
-    //   this.setState({loading: false, purchasing: false})
-    // });
-    this.props.history.push('/checkout');
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
+    }
+    queryParams.push('price=' + this.state.totalPrice);
+    const queryString = queryParams.join('&');
+    this.props.history.push({
+      pathname: '/checkout',
+      search: '?' + queryString
+    });
   }
 
   render () {
