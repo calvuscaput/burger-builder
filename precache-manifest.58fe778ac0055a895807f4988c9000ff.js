c9b856d09529d52bbc3a259b21ec6f0d39fe5dff
CalvusCaput <CalvusCaput@yandex.ru>
2018-11-22 19:38:07 +0300

    Updates

---
+++ precache-manifest.58fe778ac0055a895807f4988c9000ff.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bbd1f8fbf5b397884b2",
+    "url": "/burger-builder/static/css/3.f20ac6a1.chunk.css"
+  },
+  {
+    "revision": "ac7be8a599227501aa85",
+    "url": "/burger-builder/static/css/main.a6894593.chunk.css"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "de15c13b54b3c9371098",
+    "url": "/burger-builder/static/js/1.de15c13b.chunk.js"
+  },
+  {
+    "revision": "244546851e34e8462fea",
+    "url": "/burger-builder/static/js/runtime~main.24454685.js"
+  },
+  {
+    "revision": "4f622f9c66f767be9c48",
+    "url": "/burger-builder/static/js/2.4f622f9c.chunk.js"
+  },
+  {
+    "revision": "ac7be8a599227501aa85",
+    "url": "/burger-builder/static/js/main.ac7be8a5.chunk.js"
+  },
+  {
+    "revision": "3bbd1f8fbf5b397884b2",
+    "url": "/burger-builder/static/js/3.3bbd1f8f.chunk.js"
+  },
+  {
+    "revision": "2164056d05f8d541112a",
+    "url": "/burger-builder/static/js/4.2164056d.chunk.js"
+  },
+  {
+    "revision": "4f622f9c66f767be9c48",
+    "url": "/burger-builder/static/css/2.bcf07da8.chunk.css"
+  },
+  {
+    "revision": "de15c13b54b3c9371098",
+    "url": "/burger-builder/static/css/1.33c60f84.chunk.css"
+  },
+  {
+    "revision": "73751e05d766276854e7ba7794636919",
+    "url": "/burger-builder/index.html"
+  }
+];
